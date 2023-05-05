cdf2b7b8bdcaf37c590ab35bbafec0ab8450104b
Daiana Paz <daianadenispaz@gmail.com>
2023-05-04 21:55:41 -0300

    se agrego contador y catalogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
-import NavBar from './components/NavBar/NavBar/NavBar';
-import ItemListContaier from './components/NavBar/ItemListContainer/ItemListContainer';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContaier from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ItemListContaier greeting={'Bienvenidos'}/>
+      <ItemCount initial={1} stock={20} onAdd={(quantity) => console.log ("Cantidad agregada", quantity)} />
     </div>
   );
 }
--- src/components/NavBar/ItemListContainer/ItemListContainer.jsx
+++
@@ -1,9 +0,0 @@
-const ItemListContaier = ({greeting}) => {
-    return (
-        <div>
-            <h1>{greeting}</h1>
-        </div>
-    )
-}
-
-export default ItemListContaier
