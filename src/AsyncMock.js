63eea83e766aec6642da737be14faef1e8e34a1f
Daiana Paz <daianadenispaz@gmail.com>
2023-05-10 20:33:59 -0300

    pase las imagenes a public

--- src/AsyncMock.js
+++ src/AsyncMock.js
@@ -6,7 +6,7 @@ const products = [
         id: "1",
         name: "Royal Canin (perro)",
         price: 17711,
-        img: "./components/NavBar/CartWidget/assets/perro.png",
+        img: "/public/assets/perro.png",
         stock: 20,
         description: "Alimento Royal Canin Size Health Nutrition Maxi Adult para perro adulto de raza grande sabor mix en bolsa de 15 kg"
     },
@@ -15,7 +15,7 @@ const products = [
         id: "2",
         name: "Royal Canin (gato)",
         price: 14600,
-        img: "./components/NavBar/CartWidget/assets/gato.png",
+        img: "/public/assets/gato.png",
         stock: 15,
         description: "Alimento Royal Canin Feline Health Nutrition Fit para gato adulto sabor mix en bolsa de 7.5 kg"
     },
@@ -24,7 +24,7 @@ const products = [
         id: "3",
         name: "Pelota de goma",
         price: 1279,
-        img: "./components/NavBar/CartWidget/assets/pelota.png",
+        img: "/public/assets/pelota.png",
         stock: 30,
         description: "Pelota De Goma Tpr Limpia Dientes Para Perro 6 Cm"
     },
@@ -33,7 +33,7 @@ const products = [
         id: "4",
         name: "Dispenser alimento",
         price: 1034,
-        img: "./components/NavBar/CartWidget/assets/dispenser.png",
+        img: "/public/assets/dispenser.png",
         stock: 30,
         description: "Juguete Dispenser Alimento Gatos Snack Shaking Mascotas"
     },
@@ -42,7 +42,7 @@ const products = [
         id: "5",
         name: "Polar",
         price: 1499,
-        img: "./components/NavBar/CartWidget/assets/abrigo.png",
+        img: "/public/assets/abrigo.png",
         stock: 50,
         description: "Polar Para Mascotas Muy Abrigados Varios Colores"
     },
@@ -51,7 +51,7 @@ const products = [
         id: "6",
         name: "Buzo",
         price: 2450,
-        img: "./components/NavBar/CartWidget/assets/buzos.png",
+        img: "/public/assets/buzos.png",
         stock: 50,
         description: "Buzo Para Perros Gatos Frisado Con Dibujos Animados Mascotas"
     },
@@ -60,7 +60,7 @@ const products = [
         id: "7",
         name: "Cama",
         price: 3000,
-        img: "./components/NavBar/CartWidget/assets/cama.png",
+        img: "/public/assets/cama.png",
         stock: 30,
         description: "Moises Cuna Camita Cucha + Colchón Mascotas Perro Gato 35x45"
     },
