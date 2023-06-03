0052e790354faf5c99f47ab1655b3d684e93c990
Daiana Paz <daianadenispaz@gmail.com>
2023-06-03 12:42:42 -0300

    modificacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import { useEffect } from 'react';
 import ItemListContaier from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
